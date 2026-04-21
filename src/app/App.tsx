3d5b98218b21b7c778dc25a6bd40a3976f42aced
shemanul <shemanul@gmail.com>
2026-04-21 10:00:26 +0900

    fix subFontsize data type

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
     backgroundColor: '#fb9189',
     backgroundImage: '',
     fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif',
-    subFontSize: '1.25rem',
+    subFontSize: 20,,
     subFontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, sans-serif',
     screenSaver: false,
     particleRefresh: false,
